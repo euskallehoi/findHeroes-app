19d7768c5a1319d4fb5e2f30f49151b9e4af0ef4
leon <leonchorroarin@gmail.com>
2023-07-13 14:26:51 -0300

    algunas actualizaciones

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,8 +1,12 @@
 
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+
+    const { user } = useContext( AuthContext )
 
     const navigate = useNavigate();
     
@@ -52,7 +56,7 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className='nav-item nav-link text-primary'>
-                        Leon
+                        {user?.name}
                     </span>
 
                     <button 
